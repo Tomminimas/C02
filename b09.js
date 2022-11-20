649079b53e77b68372dd77c8a587cde709ff802b
Tomminimas <tranvietmyngoc5316@gmail.com>
2022-11-20 10:59:34 +0700

    b09

--- b09.js
+++ b09.js
@@ -1,76 +1,74 @@
-const readline = require('readline-sync');
+const readline = require("readline-sync");
 
-let a, b, n; 
-n = Number(readline.question('Nhap vao so n:  '));
-a = parseInt(a) % 10;
-b = parseInt(b) / 10;
-
+let a, b, n;
+n = Number(readline.question("Nhap vao so n:  "));
+a = parseInt(n) % 10;
+b = parseInt(n / 10);
 
 if (n < 10 || n > 99) {
-    console.log('Nhap vao hai chu so');
-}
-else {
-    switch (b) {
-        case 1: 
-            console.log('Muoi ');
-            break;
-        case 2: 
-            console.log('Hai Muoi ');
-            break;
-        case 3: 
-            console.log('Ba Muoi ');
-            break;
-        case 4: 
-            console.log('Bon Muoi ');
-            break;    
-        case 5: 
-            console.log('Nam Muoi ');
-            break;
-        case 6: 
-            console.log('Sau Muoi ');
-            break;
-        case 7: 
-            console.log('Bay Muoi ');
-            break;
-        case 8: 
-            console.log('Tam Muoi ');
-            break;
-        case 9: 
-            console.log('Chin Muoi ');
-            break;
-        default: 
-            console.log('Khong hop le');
+  console.log("Nhap vao hai chu so");
+} else {
+  switch (b) {
+    case 1:
+        s = "Muoi ";
+        break;
+    case 2:
+        s = "Hai muoi ";
+        break;
+    case 3:
+        s = "Ba muoi ";
+        break;
+    case 4:
+        s = "Bon muoi ";
+        break;
+    case 5:
+        s = "Nam muoi ";
+        break;
+    case 6:
+        s = "Sau muoi ";
+        break;
+    case 7:
+        s = "Bay muoi ";
+        break;
+    case 8:
+        s = "Tam muoi ";
+        break;
+    case 9:
+        s = "Chin muoi ";
+        break;
+    default:
+        console.log("Khong hop le");
     }
-    switch (a) {
-        case 1: 
-            console.log('Mot ');
-            break;
-        case 2: 
-            console.log('Hai ');
-            break;
-        case 3: 
-            console.log('Ba ');
-            break;
-        case 4: 
-            console.log('Bon ');
-            break;    
-        case 5: 
-            console.log('Nam ');
-            break;
-        case 6: 
-            console.log('Sau ');
-            break;
-        case 7: 
-            console.log('Bay ');
-            break;
-        case 8: 
-            console.log('Tam ');
-            break;
-        case 9: 
-            console.log('Chin ');
-            break;
-        default: 
-            console.log('Khong hop le');
+  switch (a) {
+    case 1:
+        s += "mot";
+        break;
+    case 2:
+        s += "hai";
+        break;
+    case 3:
+        s += "ba";
+        break;
+    case 4:
+        s += "bon";
+        break;
+    case 5:
+        s += "nam";
+        break;
+    case 6:
+        s += "sau";
+        break;
+    case 7:
+        s += "bay";
+        break;
+    case 8:
+        s += "tam";
+        break;
+    case 9:
+        s += "chin";
+        break;
+    default:
+      console.log("Khong hop le");
     }
 }
-console.log(n);
+console.log(s);
